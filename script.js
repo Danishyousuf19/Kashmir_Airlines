1fc6d6dce659bdab1a696931a208101bcf0d335c
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-12-22 11:00:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); 
+  setTimeout(showSlides, 2000); 
 }
 
  
